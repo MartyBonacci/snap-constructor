c323771127b4a89ef151b17ae3f3d579ee3a2c4d
Marty Bonacci <martybonacci@gmail.com>
2018-01-23 08:54:15 -0700

    cleaned it up a little

--- index.php
+++ index.php
@@ -4,28 +4,22 @@ class Car {
 	private $color;
 	private $mpg;
 	public function __construct(string $newColor, int $newMpg) {
-		try{
 			$this->setColor($newColor);
 			$this->setMpg($newMpg);
-		}
-		catch(\InvalidArgumentException | \TypeError $exception) {
-			$exceptionType = get_class($exception);
-			throw (new $exceptionType($exception-getMessage(),0, $exception));
-		}
 	}
 
 	/**
 	 * @return string
 	 */
 	public function getColor(): string {
-		return $this->color;
+		return ($this->color);
 	}
 
 	/**
 	 * @return int
 	 */
 	public function getMpg(): int {
-		return $this->mpg;
+		return ($this->mpg);
 	}
 
 	/**
@@ -48,4 +42,4 @@ $jetta = new car("blue",32);
 $jettaColor = $jetta->getColor();
 $jettaMpg = $jetta->getMpg();
 
-echo ($jettaColor ." " . $jettaMpg);
+echo ($jettaColor . " " . $jettaMpg);
